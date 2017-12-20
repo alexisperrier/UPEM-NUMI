00c37e44c92fbad4e9ceb868e2972e6d9ca392fc
Alexis Perrier <alexis.perrier@gmail.com>
2017-12-20 08:18:11 +0100

    lab03

--- lab02/notebooks/initialize.R
+++ lab02/notebooks/initialize.R
@@ -8,18 +8,24 @@
 # install.packages( c("stringr", "RColorBrewer", "stringr"), dependencies = TRUE)
 # install.packages( c("geometry", "Rtsne", "GetoptLong"), dependencies = TRUE )
 
+install.packages('igraph')
+install.packages('stm')
+install.packages('stmBrowser')
+
 
 # charger les packages suivants
 library('stringr')
-library('stm')
-library('stmBrowser')
 library('wordcloud')
-library('igraph')
 library('geometry')
 library('Rtsne')
 library('data.table')
 library('SnowballC')
 library('GetoptLong')
+
+library('igraph')
+library('stm')
+library('stmBrowser')
+
 
 # formatter le print
 qq.options("cat_prefix" = function(x) format(Sys.time(), "\n[%H:%M:%S] "))
